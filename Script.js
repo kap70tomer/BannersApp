741c3458c50af0aa7a7f5307ae36552a1e17383e
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2022-02-09 15:51:09 +0200

    added Form cleanUp function

--- Script.js
+++ Script.js
@@ -1,9 +1,15 @@
 // @@ func - createNewBanner, takes in 3 paramethers to create a new 'Banner' element.
-const createNewBanner = (element_id, redirect_link, banner_img) => {
-    //Insert a banner element to DOM with given Args.
+const createNewBanner = (element_id, redirect_link, banner_img, event) => {
+    //Prevent Deafult Submit refresh behavior.
+    event.preventDefault();
+    //Banners Factory function returns new Banner HTML Element.
     let be = bannersElementFactory(redirect_link, banner_img);
-    insertBannerToDOM(element_id, be);
-}
+    //Insert a banner element to DOM with given id.
+    insertBannerByElementId(element_id, be);
+    //Clean up func for form fields.
+    initializeForm();
+};
+
 // @@ func - Create a banner DOM element and set 'Click' event listener,
 // return - new banner Element.
 const bannersElementFactory = (redirect_link, banner_img) => {
@@ -16,11 +22,16 @@ const bannersElementFactory = (redirect_link, banner_img) => {
     bannerElement.style.width = '320px';
     //Add Event listener to invoke redirection by Click on UI.
     bannerElement.addEventListener('click', () => window.open(redirect_link, '_blank'));
-    // returns the created Element.
+    //return the new html Element.
     return bannerElement;
-}
+};
+
 // @@ func - Takes in banner element to insert as child of given DOM element by its id.
 const insertBannerByElementId = (element_id, bannerElement) => {
     //Append created banner to the requested element by element's id
     document.getElementById(element_id).appendChild(bannerElement);
-}
+};
+// @@ func - Clear the banner-form fields.
+const initializeForm =()=>{
+    document.forms['banner-form'].reset();
+};
